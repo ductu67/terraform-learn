3a78e771a00512c9e5a931ad4ddf3e9f7945d49e
Nguyen Duc Tu <88696492+TuND-MOR@users.noreply.github.com>
2024-04-26 17:57:04 +0700

    Terraform 6 infra app

---
+++ terraform-6-infra-app/modules/networking/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  type    = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+}
+
+variable "private_subnets" {
+  type    = list(string)
+}
+
+variable "public_subnets" {
+  type    = list(string)
+}
+
+variable "database_subnets" {
+  type    = list(string)
+}
