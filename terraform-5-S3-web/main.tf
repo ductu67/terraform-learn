1b6dee7c1b29f4feba8fb4c56dab13ea43ea3f31
Nguyen Duc Tu <88696492+TuND-MOR@users.noreply.github.com>
2024-04-26 10:18:04 +0700

    Terraform with s3 web

---
+++ terraform-5-S3-web/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.44.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "static" {
+  bucket        = "terraform-series-s3-web"
+  force_destroy = true
+
+  # tags = local.tags
+}
+
+resource "aws_s3_bucket_acl" "static" {
+  bucket = aws_s3_bucket.static.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "static" {
+  bucket = aws_s3_bucket.static.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+data "aws_iam_policy_document" "static" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static.arn}/*"]
+
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = file("s3_static_policy.json")
+}
+
+locals {
+  mime_types = {
+    html  = "text/html"
+    css   = "text/css"
+    ttf   = "font/ttf"
+    woff  = "font/woff"
+    woff2 = "font/woff2"
+    js    = "application/javascript"
+    map   = "application/javascript"
+    json  = "application/json"
+    jpg   = "image/jpeg"
+    png   = "image/png"
+    svg   = "image/svg+xml"
+    eot   = "application/vnd.ms-fontobject"
+  }
+}
+
+resource "aws_s3_object" "object" {
+  for_each = fileset(path.module, "static-web/**/*")
+  bucket = aws_s3_bucket.static.id
+  key    = replace(each.value, "static-web", "")
+  source = each.value
+  etag         = filemd5("${each.value}")
+  content_type = lookup(local.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
+}
